002ff63b23aff05a03544903fe35ad7e5dd72b39
blsonalib <blsonalib@gmail.com>
2019-11-22 09:21:41 +0530

    Add : assign latter to the player

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -x
 echo "**********Welcome to TicTacToe Game**********"
+#canstants
+PLAYER="X"
+COMPUTER="O"
+
 declare -A boardOfTicTacToeGame
 
 function resetTheBoardOfGame()
 { 
-   numberOfBlocks=9 
-   for (( board=1; board<=numberOfBlocks ; board++ ))
+    numberOfBlocks=9 
+    for (( board=1; board<=numberOfBlocks ; board++ ))
     do
-        boardOfTicTacToeGame[$board]="-"
-   done
+          boardOfTicTacToeGame[$board]="-"
+    done
 }
 resetTheBoardOfGame
 echo ${boardOfTicTacToeGame[@]}
+
