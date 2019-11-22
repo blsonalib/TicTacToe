d40587d282c2fe68f612dfd3ff324fc19c0516c8
blsonalib <blsonalib@gmail.com>
2019-11-22 09:16:11 +0530

    Add: reset the board

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,2 +1,14 @@
 #!/bin/bash -x
 echo "**********Welcome to TicTacToe Game**********"
+declare -A boardOfTicTacToeGame
+
+function resetTheBoardOfGame()
+{ 
+   numberOfBlocks=9 
+   for (( board=1; board<=numberOfBlocks ; board++ ))
+    do
+        boardOfTicTacToeGame[$board]="-"
+   done
+}
+resetTheBoardOfGame
+echo ${boardOfTicTacToeGame[@]}
